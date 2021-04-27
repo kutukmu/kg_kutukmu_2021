223e3169d1cd28821a98e92c1cc1b6401333013f
Kerim Kutuk <kutukmu@gmail.com>
2021-04-27 11:52:01 -0500

    final result

--- features/readNumbers.go
+++ features/readNumbers.go
@@ -29,3 +29,22 @@ func ConvertStringToIntArr(str *string) []int{
 	return numbersArr
 
 }
+
+func ConvertToPhonetic(numbers []int)string {
+	obj := map[int]string{0:"Zero",1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 6:"Six", 7:"Seven", 8:"Eight", 9:"Nine"}
+	arr := []string{}
+	for _,j := range numbers{
+		var str string = ""
+		val := j
+		for val > 0{
+			digit := val % 10
+			val = val /10
+			str = obj[digit] + str
+			
+		}
+		arr = append(arr, str)
+		
+	}
+
+	return strings.Join(arr[:], ",")
+}
--- main.go
+++ main.go
@@ -1,34 +1,18 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
+
+	"github.com/kutukmu/kg_kutukmu_2021/features"
 )
 
 
 func main() {
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter the numbers: ")
-	numbers, _ := reader.ReadString('\n')
-	stringArr := strings.Fields(numbers)
-	var numbersArr = []int{}
-	for _,i := range stringArr{
-		j,err := strconv.Atoi(i)
-		if err != nil{
-			panic(err)
-		}else{
-			numbersArr = append(numbersArr, j)
-		}
-	}
+	fmt.Println("Please start entering:")
 
-	for _,j := range numbersArr{
-		fmt.Printf("You entered %d but current is %d \n",j,j+1)
-	}
-
-	
-
+	str :=features.ReadNumbers()
+	numArr := features.ConvertStringToIntArr(str)
+	result := features.ConvertToPhonetic(numArr)
+	fmt.Print(result)
 }
