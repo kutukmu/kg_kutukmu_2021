07a4df6e450515f627c4886c26883f802f6e8d82
Kerim Kutuk <kutukmu@gmail.com>
2021-04-27 11:05:07 -0500

    ConverStringtoIntArr

--- features/readNumbers.go
+++ features/readNumbers.go
@@ -3,11 +3,29 @@ package features
 import (
 	"bufio"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func ReadNumbers() *string{
 	reader := bufio.NewReader(os.Stdin)
-	numbers, _ := reader.ReadString('\n')
-	return &numbers
+	str, _ := reader.ReadString('\n')
+	return &str
 }
 
+func ConvertStringToIntArr(str *string) []int{
+
+	stringsArr := strings.Fields(*str)
+	numbersArr := []int{}
+	for _,j := range stringsArr{
+		i,err := strconv.Atoi(j)
+		if err != nil{
+			panic(err)
+		}else{
+			numbersArr = append(numbersArr, i)
+		}
+	}
+
+	return numbersArr
+
+}
